c5cd6e5526548688040e078b1036fa22f91cd06d
Maclixiaochun <wozhizui1990@163.com>
2016-09-06 21:39:51 +0800

    over 以非阻塞操作进行请求响应

--- The Node Beginner Book/requestHandlers.js
+++ The Node Beginner Book/requestHandlers.js
@@ -1,18 +1,20 @@
 var exec = require("child_process").exec;
 
-function start() {
+function start(response) {
   console.log("Request handler 'start' was called.");
-  var content = "empty";
 
-  exec("ls -lah",function(error,stdout,stderr){
-    content = stdout;
+  exec("find /",function(error,stdout,stderr){
+    response.writeHead(200,{"Content-Type":"text/plain"});
+    response.write(stdout);
+    response.end();
   });
-  return content;
 }
 
-function upload() {
+function upload(response) {
   console.log("Request handler 'upload' was called.");
-  return "Hello Upload!";
+  response.writeHead(200,{"Content-Type":"text/plain"});
+  response.write("Hello Upload");
+  response.end();
 }
 
 exports.start = start;
