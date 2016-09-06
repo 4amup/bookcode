c9ce752fe8a7fefa514e6885a5f880b2472dcd7d
Maclixiaochun <wozhizui1990@163.com>
2016-09-06 22:23:09 +0800

    over 处理POST请求

--- The Node Beginner Book/requestHandlers.js
+++ The Node Beginner Book/requestHandlers.js
@@ -1,19 +1,29 @@
-var exec = require("child_process").exec;
-
-function start(response) {
+function start(response,postData) {
   console.log("Request handler 'start' was called.");
+  
+  var body = '<html>'+
+    '<head>'+
+    '<meta http-equiv="Content-Type" content="text/html; '+
+    'charset=UTF-8" />'+
+    '</head>'+
+    '<body>'+
+    '<form action="/upload" method="post">'+
+    '<textarea name="text" rows="20" cols="60"></textarea>'+
+    '<input type="submit" value="Submit text" />'+
+    '</form>'+
+    '</body>'+
+    '</html>';
 
-  exec("find /",function(error,stdout,stderr){
-    response.writeHead(200,{"Content-Type":"text/plain"});
-    response.write(stdout);
+    response.writeHead(200,{"Content-Type":"text/html"});
+    response.write(body);
     response.end();
-  });
 }
 
-function upload(response) {
+function upload(response,postData) {
   console.log("Request handler 'upload' was called.");
   response.writeHead(200,{"Content-Type":"text/plain"});
-  response.write("Hello Upload");
+  response.write("You've sent the text:"+
+  querystring.parse(postData).text);
   response.end();
 }
 
