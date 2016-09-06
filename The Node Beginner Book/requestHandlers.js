70c87395277117dc960afce83330faa1500a1aae
Maclixiaochun <wozhizui1990@163.com>
2016-09-06 21:17:11 +0800

    over 阻塞与非阻塞

--- The Node Beginner Book/requestHandlers.js
+++ The Node Beginner Book/requestHandlers.js
@@ -1,13 +1,13 @@
+var exec = require("child_process").exec;
+
 function start() {
   console.log("Request handler 'start' was called.");
-  
-  function sleep(milliSeconds) {
-    var startTime = new Date().getTime();
-    while(new Date().getTime()<startTime+milliSeconds);
-  }
+  var content = "empty";
 
-  sleep(10000);
-  return "Hello Start!";
+  exec("ls -lah",function(error,stdout,stderr){
+    content = stdout;
+  });
+  return content;
 }
 
 function upload() {
