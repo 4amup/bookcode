355a7578673004831fb6d4acb754d1563ea29a09
wozhizuipc <wozhizui1990@163.com>
2016-09-06 11:24:53 +0800

    over 路由给真正的请求处理程序
    1.路由记得是一个名词
    2.函数式编程的含义是动作化，动词化
    3.下一节 让请求程序作出响应

--- The Node Beginner Book/server.js
+++ The Node Beginner Book/server.js
@@ -1,12 +1,18 @@
 var http = require('http');
+var url = require('url');
 
-function start() {
+function start(route,handle) {
   function onRequest(request,response){
-    console.log('Request received.');
+    var pathname = url.parse(request.url).pathname;
+    console.log('Request for '+pathname+' received.');
+
+    route(handle,pathname);
+
     response.writeHead(200,{'Content-Type':'text/plain'});
     response.write('Hello World');
     response.end();
   }
+  
   http.createServer(onRequest).listen(4444);
   console.log('Server has started.')
 }
