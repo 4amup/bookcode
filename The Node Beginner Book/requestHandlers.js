d496678412ef5e9536c63210560864d3c1f6dcb2
Maclixiaochun <wozhizui1990@163.com>
2016-09-06 23:32:41 +0800

    update

--- The Node Beginner Book/requestHandlers.js
+++ The Node Beginner Book/requestHandlers.js
@@ -1,17 +1,19 @@
-var querystring = require("querystring");
+var querystring = require("querystring"),
+    fs = require("fs");
 
-function start(response,postData) {
+function start(response, postData){
   console.log("Request handler 'start' was called.");
-  
-  var body = '<html>'+
+
+  var body ='<html>'+
     '<head>'+
-    '<meta http-equiv="Content-Type" content="text/html; '+
-    'charset=UTF-8" />'+
+    '<meta http-equiv="Content-Type" '+
+    'content="text/html; charset=UTF-8" />'+
     '</head>'+
     '<body>'+
-    '<form action="/upload" method="post">'+
-    '<textarea name="text" rows="20" cols="60"></textarea>'+
-    '<input type="submit" value="Submit text" />'+
+    '<form action="/upload" enctype="multipart/form-data" '+
+    'method="post">'+
+    '<input type="file" name="upload">'+
+    '<input type="submit" value="Upload file" />'+
     '</form>'+
     '</body>'+
     '</html>';
@@ -21,13 +23,29 @@ function start(response,postData) {
     response.end();
 }
 
-function upload(response,postData) {
+function upload(response, postData){
   console.log("Request handler 'upload' was called.");
   response.writeHead(200,{"Content-Type":"text/plain"});
-  response.write("You've sent the text:"+
+  response.write("You've sent the text: "+
   querystring.parse(postData).text);
   response.end();
 }
 
+function show(response, postData){
+  console.log("Request handler 'show' was called.");
+  fs.readFile("/tmp/test.png","binary",function(error, file){
+    if(error){
+      response.writeHead(500,{"Content-Type":"text/plain"});
+      response.write(error +"\n");
+      response.end();
+    }else{
+      response.writeHead(200,{"Content-Type":"image/png"});
+      response.write(file,"binary");
+      response.end();
+    }
+  });
+}
+
 exports.start = start;
 exports.upload = upload;
+exports.show = show;
