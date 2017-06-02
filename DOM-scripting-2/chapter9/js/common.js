6a64ef2ac363a1dfa95d3d33dc9091def6356786
wozhizuipc <490398311@qq.com>
2017-06-02 14:29:44 +0800

    over 分离css后

--- DOM-scripting-2/chapter9/js/common.js
+++ DOM-scripting-2/chapter9/js/common.js
@@ -1,6 +1,15 @@
-var paras = document.getElementsByTagName("p");
-for(let i=0; i<paras.length; i++) {
-  paras[i].onclick = function() {
-    alert("You clicked on a paragraph.");
+window.onload = function () {
+  var paras = document.getElementsByTagName("p");
+  for(let i=0; i<paras.length; i++) {
+    paras[i].onclick = function() {
+      alert("You clicked on a paragraph.");
+    }
   }
+
+  let para = document.getElementById("example");
+  alert(typeof para.nodeName);
+  alert(typeof para.style);
+  alert(`The color is ${para.style.color}`);
+  alert(`The font family is ${para.style.fontFamily}`);
+  alert(`The font size is ${para.style.fontSize}`);
 }
