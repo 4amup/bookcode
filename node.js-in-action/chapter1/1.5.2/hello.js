36251ed4ad3f7b885b66216059f64f2053790d6f
wozhizuipc <490398311@qq.com>
2017-06-04 14:58:52 +0800

    用事件监听的样式写HTTP服务器

--- node.js-in-action/chapter1/1.5.2/hello.js
+++ node.js-in-action/chapter1/1.5.2/hello.js
@@ -1,6 +1,9 @@
 var http = require('http');
-http.createServer((req, res) => {
+var server = http.createServer();
+// 设置事件监听器
+server.on('request', (req, res) => {
   res.writeHead(200, {'Content-Type': 'text/plain'});
   res.end('Hello World\n');
-}).listen(3000);
+})
+server.listen(3000);
 console.log('Server running at http://localhost:3000/');
