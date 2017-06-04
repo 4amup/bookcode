aaed798e3b31646efd5f9bdd66999c4760873722
wozhizuipc <490398311@qq.com>
2017-06-04 15:17:14 +0800

    over chapter1 use stream&pipe

--- node.js-in-action/chapter1/1.5.3-stream/app.js
+++ node.js-in-action/chapter1/1.5.3-stream/app.js
@@ -1,8 +1,8 @@
+var http = require('http');
 var fs = require('fs');
-var stream = fs.createReadStream('./resource.json');
-stream.on('data', (chunk) => {
-  console.log(chunk);
-})
-stream.on('end', () => {
-  console.log('finished');
-})
+http.createServer((req, res) => {
+  res.writeHead(200, {'Content-Type': 'image/png'});
+  fs.createReadStream('./image.png').pipe(res);
+}).listen(3000);
+
+console.log('Server running at http://localhost:3000/');
