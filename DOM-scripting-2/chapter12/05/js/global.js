9d33ea025ad4c0ed2cd4a945fcd1deffcac7f21b
wozhizuipc <490398311@qq.com>
2017-06-07 11:37:45 +0800

    over the 验证函数

--- DOM-scripting-2/chapter12/05/js/global.js
+++ DOM-scripting-2/chapter12/05/js/global.js
@@ -298,10 +298,43 @@ function resetFields(form) {
     element.onblur();
   }
 }
+// 表单验证函数
+function isFilled(field) {
+  if(field.value.replace(' ', '').length === 0) return;
+  let placeholder = field.placeholder || field.getAttribute('placeholder');
+  return (field.value != placeholder);
+}
+function isEmail(field) {
+  return (field.value.indexOf("@") != -1 && field.value.indexOf('.') != -1); 
+}
+function validateForm(form) {
+  for(let i=0; i<form.elements.length; i++) {
+    let element = form.elements[i];
+    if(element.required) {
+      if(!isfilled(element)) {
+        alert(`Please fill in the ${element.name}field.`);
+        return;
+      }
+    }
+    if(element.type = 'email') {
+      if(!isEmail(element)) {
+        if(!isEmail(element)) {
+          alert(`The ${element.name}field must be a valid email address.`);
+          return;
+        }
+      }
+    }
+  }
+  return true;
+}
+
 function prepareForms() {
   for(let i=0; i<document.forms.length; i++) {
     let thisform = document.forms[i];
     resetFields(thisform);
+    thisform.onsubmit = function() {
+      return validateForm(this);
+    }
   }
 }
 addLoadEvent(prepareSlideshow);
@@ -315,4 +348,6 @@ addLoadEvent(highlightRows);
 addLoadEvent(displayAbbreviations);
 // contact.html
 addLoadEvent(focusLabels);
+addLoadEvent(prepareForms);
+    // 表单验证
 addLoadEvent(prepareForms);
