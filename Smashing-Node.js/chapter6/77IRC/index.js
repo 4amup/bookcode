dd3c2a941f0a2f0fbfac7f3932dcce6987a00842
wozhizuipc <490398311@qq.com>
2017-06-15 13:59:34 +0800

    over the chapter6 IRC没有实际运行起来

---
+++ Smashing-Node.js/chapter6/77IRC/index.js
@@ -0,0 +1,10 @@
+const net = require('net');
+
+let client = net.createConnection(6667, 'irc.freenode.net')
+client.setEncoding('utf-8')
+
+client.on('connect', function () {
+  client.write('NICK mynick\r\n');
+  client.write('USER mynick 0 * :realname\r\n');
+  client.write('JOIN #node.js\r\n');
+});
