14e8f1cddb43ee3e8c53dcad7169c2d66621c4f1
wozhizuipc <490398311@qq.com>
2017-06-16 11:26:04 +0800

    over 豆瓣读书API HTTPS请求协议 解析出书名

---
+++ Smashing-Node.js/chapter7/100/douban.js
@@ -0,0 +1,35 @@
+const https = require('https'),
+      qs = require('querystring');
+
+let search = process.argv[2];
+console.log(search);
+
+if(!search.length) {
+  return console.log('\nUsage: node douban <search>\n');
+}
+
+console.log(`\nsearching for: ${search}...`);
+let option = {
+  host: 'api.douban.com',
+  path: '/v2/book/search?' + qs.stringify({q: search})
+};
+
+let req = https.request(option, (res) => {
+  let body = '';
+  res.setEncoding('utf8');
+  
+  res.on('data', (chunk) => {
+    body += chunk;
+  });
+  
+  res.on('end', () => {
+    let obj = JSON.parse(body);
+
+    obj.books.forEach((book) => {
+      console.log('--');
+      console.log(book.title);
+    });
+  });
+});
+
+req.end();
