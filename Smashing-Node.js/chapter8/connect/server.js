cccff850d9c505df54c979bfd225cc26f8aff2a1
wozhizuipc <490398311@qq.com>
2017-06-22 14:29:15 +0800

    重新写了112页的内容，符合书中内容

--- Smashing-Node.js/chapter8/connect/server.js
+++ Smashing-Node.js/chapter8/connect/server.js
@@ -3,13 +3,10 @@ const static = require('serve-static');
 
 let app = connect();
 
+// 这个中间件可以将制定目录下的静态文件，从文件系统放到服务器上
 app.use(static(__dirname + '/website'));
 
-app.use((req, res) => {
-  res.setHeader('Content-Type', 'text/html');
-  res.end('<h1>Hello connect!</h1>');
-});
-
+// 利用connect自带的API监听3000端口
 app.listen(3000);
 
 console.log('server run at http://localhost:3000');
