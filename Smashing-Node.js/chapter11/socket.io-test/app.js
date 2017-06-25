f61d83f69b02659bbbab2a1ab6d1b5fcf7a48e31
wozhizuipc <490398311@qq.com>
2017-06-25 14:37:41 +0800

    done connection和disconnect事件

--- Smashing-Node.js/chapter11/socket.io-test/app.js
+++ Smashing-Node.js/chapter11/socket.io-test/app.js
@@ -1,11 +1,25 @@
 const express = require('express'),
-      http = require('http');
+      http = require('http'),
+      socket = require('socket.io');
 
+// 实例初始化过程
 let app = express();
 let server = http.Server(app);
+let io = socket(server);
 
+// 简单路由返回一个首页
 app.get('/', (req, res) => {
-  res.send('<h1>Hello world</h1>');
+  // res.sendFile()是express4的一个API
+  res.sendFile(__dirname + '/index.html');
+});
+
+// 添加socket.io的连接监听
+io.on('connection', (socket) => {
+  console.log('a user connected');
+  // 监听断开事件
+  socket.on('disconnect', () => {
+    console.log('user disconnected');
+  });
 });
 
 server.listen(3000, () => {
