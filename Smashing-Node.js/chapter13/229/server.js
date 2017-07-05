24375d23475c6026a203684a91c75c5ed1189e88
wozhizuipc <490398311@qq.com>
2017-07-05 12:46:50 +0800

    hang  数据库报错不会用了 ，下一阶段用koa2和sequelize v4再重写吧。

--- Smashing-Node.js/chapter13/229/server.js
+++ Smashing-Node.js/chapter13/229/server.js
@@ -7,7 +7,15 @@ const express = require('express')
 let app = express()
 
 // 初始化sequelize
-const sequelize = new Sequelize('todo-example', 'root', '8307')
+const sequelize = new Sequelize('todo_example', 'root', '8307', {
+  host: 'localhost',
+  dialect: 'mysql',
+  pool: {
+    max: 5,
+    min: 0,
+    idle: 10000
+  }
+});
 // 定义项目和工程模型
 const Project = sequelize.define('Project', {
   title: Sequelize.STRING,
@@ -21,10 +29,10 @@ Task.belongsTo(Project)
 Project.hasMany(Task)
 // belongsTo联合意味着每个Task都有一个指向它所属项目的字段。另外每个任务模型都会有一个名为getProject的方法来获取其所属的项目。
 // 对于hasMany而言，调用find查询到项目后，它们都会有一个名为getTasks的方法来获取项目中的任务。
-// 初次之外，sequelize还支持另一种关系：hasOne。不过本例中用不到这种联合，他与belongsTo是相对的。
+// 初次之外，sequelize还支持另一种关系：hasOne。
 
 // 同步
-sequelize.sync({force: true});
+sequelize.sync()
 // 创建数据
 
 
@@ -41,7 +49,10 @@ app.use(bodyParser.urlencoded({extended: false}))
 
 // 首页路由
 app.get('/', (req, res, next) => {
-  res.render('index')
+  Project.findAll()
+  .then((projects) => {
+    res.render('index', {projects: projects})
+  })
 })
 
 // 删除项目路由
@@ -52,11 +63,10 @@ app.delete('/project/:id', (req, res, next) => {
 // 创建项目路由
 app.post('/projects', (req, res, next) => {
   // 是一个promise对象
-  Project.build(req.body).save()
-  .success((obj) => {
+  Project.create(req.body)
+  .then((obj) => {
     res.send(obj) //发送JSON数据
   })
-  .error(next)
 })
 
 // 展示指定项目中的任务
@@ -66,12 +76,11 @@ app.get('/project/:id/tasks', (req, res, next) => {
 
 // 为指定项目添加任务
 app.post('/project/:id/tasks', (req, res, next) => {
-  res.body.ProjectId = req.param.id
-  Task.build(req.body).save()
-  .success((obj) => {
+  res.body.ProjectId = req.params.id
+  Task.create(req.body)
+  .then((obj) => {
     res.send(obj) //发送JSON数据
   })
-  .error(next)
 })
 
 // 删除任务路由
