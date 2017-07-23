3011323f29ac2945587c09f7abb52a52a6192828
wozhizuipc <490398311@qq.com>
2017-07-23 16:04:33 +0800

    update 加入了服务器端的逻辑，函数待完善。

--- node.js-in-action/chapter2/page23/lib/chat_server.js
+++ node.js-in-action/chapter2/page23/lib/chat_server.js
@@ -9,7 +9,22 @@ let currentRoom = {};
 
 module.exports = {
   listen: function (server) {
-    io = socket.io.listen(server);
+    let io = socketio.listen(server);
     // io.set('log level', 1); 书中的这个方法已经不再支持
+    io.sockets.on('connection', (socket) => {
+      console.log('connection!')
+      guestNumber = assignGuestName (socket, guestNumber, nickName, namesUesd);
+      joinRoom(socket, 'Lobby');
+
+      handleMessageBroadcasting(socket, nickName);
+      handleNameChangeAttempts(socket, nickName, namesUesd);
+      handleRoomJoining(socket);
+
+      ioserver.on('rooms', () => {
+        socket.emit('room', io.sockets.manager.rooms);
+      });
+
+      handleClientDisconnection(socket, nickName, namesUesd);
+    });
   }
 }
