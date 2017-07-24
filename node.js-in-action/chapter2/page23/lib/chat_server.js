ac52c3227885a71a87ba6bab9b570b421fc0ec32
wozhizuipc <490398311@qq.com>
2017-07-24 13:24:47 +0800

    update 添加了加入房间函数和自动分配name的函数。

--- node.js-in-action/chapter2/page23/lib/chat_server.js
+++ node.js-in-action/chapter2/page23/lib/chat_server.js
@@ -1,7 +1,7 @@
 // 初始化一些聊天状态变量
 let io;
 let guestNumber = 1;
-let nickName = {};
+let nickNames = {};
 let namesUesd = [];
 let currentRoom = {};
 
@@ -18,18 +18,57 @@ module.exports = {
         console.log(data);
       });
       // 以下为功能函数部分
-      // guestNumber = assignGuestName (socket, guestNumber, nickName, namesUesd);
+      guestNumber = assignGuestName (socket, guestNumber, nickNames, namesUesd);
       // joinRoom(socket, 'Lobby');
 
-      // handleMessageBroadcasting(socket, nickName);
-      // handleNameChangeAttempts(socket, nickName, namesUesd);
+      // handleMessageBroadcasting(socket, nickNames);
+      // handleNameChangeAttempts(socket, nickNames, namesUesd);
       // handleRoomJoining(socket);
 
       // ioserver.on('rooms', () => {
       //   socket.emit('room', io.sockets.manager.rooms);
       // });
 
-      // handleClientDisconnection(socket, nickName, namesUesd);
+      // handleClientDisconnection(socket, nickNames, namesUesd);
     });
+  }
+}
+
+// 自动分配name的函数
+function assignGuestName (socket, guestNumber, nickNames, namesUesd) {
+  let name = 'Guest' + guestNumber;
+  nickNames[socket.id] = name;
+  socket.emit('nameResult', {
+    sucess: true,
+    name: name
+  });
+
+  namesUesd.push(name);
+  return guestNumber + 1;
+}
+
+// 加入房间事件函数
+function joinRoom (socket, room) {
+  socket.join(room);
+  currentRoom[socket.id] = room;
+  socket.emit('joinResult', {room: room});
+  socket.broadcast.to(room).emit('message', {
+    text: `${nickNames[socket.id]} has joined ${room}.`
+  });
+  
+  let usersInRoom = io.sockets.clients(room);
+  if (usersInRoom.length>1) {
+    let usersInRoomSummary = `Users currently in ${room}: `;
+    for (let index in usersInRoomSummary) {
+      let userSocketId = usersInRoom[index].id;
+      if(userSocketId != socket.id) {
+        if (index>0) {
+          usersInRoomSummary += ', ';
+        }
+        usersInRoomSummary += nickNames[userSocketId];
+      }
+    }
+    usersInRoomSummary += '.';
+    socket.emit('message', {text: usersInRoomSummary});
   }
 }
