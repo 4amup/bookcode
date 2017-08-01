bf06654e1e933230b1352181cdc0f61ae6ff2e70
wozhizuipc <490398311@qq.com>
2017-08-01 11:04:47 +0800

    update 安装了模块，在npm官网查看了相关文档

---
+++ node.js-in-action/chapter3/page57/random_rss.js
@@ -0,0 +1,10 @@
+const fs = require('fs'),
+      request = require('request'),
+      htmlparser = require('htmlparser'),
+      configFilename = './rss_feeds.txt'
+
+// 先要检查存在这个rss文件不
+function checkForRSSFile () {
+  //
+}
+
