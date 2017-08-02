61f5e781ea935d9bf1099922d7cd714200f7a665
wozhizuipc <490398311@qq.com>
2017-08-02 16:38:36 +0800

    update 先发制人实现错误处理，fs.stat

--- node.js-in-action/chapter4/page74/static_server.js
+++ node.js-in-action/chapter4/page74/static_server.js
@@ -11,11 +11,26 @@ let server = http.createServer((req, res) => {
   // 得到解析的url对象
   let url = parse(req.url)
   let path = join(root, url.pathname) //通过url得到文件路径
-  let stream = fs.createReadStream(path)
-  stream.pipe(res) // 使用水管精简了代码
-  stream.on('error', (err) => {
-    res.statusCode = 500
-    res.end('Internal Server Error')
+
+  // 检查文件是否存在，并在响应中提供Content-Length
+  fs.stat(path, (err, stats) => {
+    if (err) {
+      if ('ENOENT' === err.code) {
+        res.statusCode = 404
+        res.end('Not Found')
+      } else {
+        res.statusCode = 500
+        res.end('Internal Server Error')
+      }
+    } else {
+      res.setHeader('Content-Length', stats.size)
+      let stream = fs.createReadStream(path)
+      stream.pipe(res)
+      stream.on('error', (err) => {
+        res.statusCode = 500
+        res.end('Internal Server Error')
+      })
+    }
   })
 })
 
