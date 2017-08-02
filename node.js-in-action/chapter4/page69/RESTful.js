825a4b96fc07adeb61a99426dc82da41adca9527
wozhizuipc <490398311@qq.com>
2017-08-02 15:37:18 +0800

    update GET，使用的是postman测试

--- node.js-in-action/chapter4/page69/RESTful.js
+++ node.js-in-action/chapter4/page69/RESTful.js
@@ -4,7 +4,7 @@ const http = require('http'),
 let items = []
 
 // 创建服务器
-let server = http.cerateServer((req, res) => {
+let server = http.createServer((req, res) => {
   switch (req.method) {
     case 'POST': {
       // 为这个post设置缓存
@@ -18,7 +18,18 @@ let server = http.cerateServer((req, res) => {
         items.push(item)
         res.end('OK\n')
       })
+      break
     }
-    break
+    case 'GET': {
+      items.forEach((item, index) => {
+        res.write(`${index}) ${item}\n`)
+      })
+      res.end()
+      break
+    }
   }
+})
+
+server.listen(3008, () => {
+  console.log('run at http://localhost:3008')
 })
