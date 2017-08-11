4cbd9d3eeff8ce00f7d510a530eecb9b52ad1a90
wozhizuipc <490398311@qq.com>
2017-08-11 14:12:06 +0800

    done 删除文档功能添加

--- node.js-in-action/chapter5/page108/app.js
+++ node.js-in-action/chapter5/page108/app.js
@@ -29,15 +29,20 @@ let Task = mongoose.model('Task', Tasks)
 // })
 
 // 更新文档
-Task.update(
-  {_id: '598d41772c4a67304873cbf6'}, //使用的是内部id更新
-  {description: 'Paint the bikeshed green.'},
-  {multi: false}, // 只更新一个文档
-  (err, raw) => {
-    if (err) throw err
-    console.log('The raw response from Mongo was ', raw)
-  }
-)
+// Task.update(
+//   {_id: '598d41772c4a67304873cbf6'}, //使用的是内部id更新
+//   {description: 'Paint the bikeshed green.'},
+//   {multi: false}, // 只更新一个文档
+//   (err, raw) => {
+//     if (err) throw err
+//     console.log('The raw response from Mongo was ', raw)
+//   }
+// )
+
+// 删除文档
+Task.findById('598d41772c4a67304873cbf6', (err, task) => {
+  task.remove()
+})
 
 // 终止数据库的连接
 mongoose.disconnect()
