a0182a4b81ec8c0141f00e2ee00f10b2a48f651c
wozhizuipc <490398311@qq.com>
2017-08-11 12:32:06 +0800

    done 按照官方文档写的

--- node.js-in-action/chapter5/page105/mongodb_server.js
+++ node.js-in-action/chapter5/page105/mongodb_server.js
@@ -1,9 +1,27 @@
-const mongodb = require('mongodb')
-let MongoClient = mongodb.MongoClient
-let url = 'mongodb://localhost:27017/myproject'
+var MongoClient = require('mongodb').MongoClient
+, assert = require('assert');
 
-MongoClient.connect(url, (err, db) => {
-  console.log('Connected correctly to server...')
+// Connection URL 
+var url = 'mongodb://localhost:27017/hello';
+// Use connect method to connect to the Server 
+MongoClient.connect(url, function(err, db) {
+assert.equal(null, err);
+console.log("Connected correctly to server");
 
-  db.close()
-})
+insertDocuments(db, function() {
+  db.close();
+});
+});
+
+// 插入函数
+var insertDocuments = function(db, callback) {
+  // Get the documents collection 
+  var collection = db.collection('documents');
+  // Insert some documents 
+  collection.insertMany([
+    {a : 1}, {a : 2}, {a : 3}
+  ], function(err, result) {
+    console.log("Inserted 3 documents into the document collection");
+    callback(result);
+  });
+}
