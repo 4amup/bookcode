ab818c4dd663b73abdd22c5950d0710704f53ef3
wozhizuipc <490398311@qq.com>
2017-08-11 14:42:24 +0800

    done 完成记录日志的中间件

--- node.js-in-action/chapter6/page112/app.js
+++ node.js-in-action/chapter6/page112/app.js
@@ -1,4 +1,14 @@
 const connect = require('connect')
 const app = connect()
 
-app.listen(3000)
+// 使用中间件
+app.use(logger)
+app.listen(3000, () => {
+  console.log('http://localhost:3000')
+})
+
+// 做日志的中间件
+function logger (req, res, next) {
+  console.log('%s %s', req.method, req.url)
+  next()
+}
