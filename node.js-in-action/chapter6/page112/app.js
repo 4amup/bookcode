e823b9f0ca5c49937c6d1b640a42a871bc69e058
wozhizuipc <490398311@qq.com>
2017-08-11 15:33:24 +0800

    done 测试完

--- node.js-in-action/chapter6/page112/app.js
+++ node.js-in-action/chapter6/page112/app.js
@@ -3,6 +3,8 @@ const app = connect()
 
 // 使用中间件
 app.use(logger)
+app.use('/admin', restrict)
+app.use('/admin', admin)
 app.use(hello)
 
 // 监听3000端口
@@ -20,4 +22,39 @@ function logger (req, res, next) {
 function hello (req, res) {
   res.setHeader('Content-Type', 'text/plain')
   res.end('hello world')
+}
+
+// Basic认证的中间件
+function restrict(req, res, next) {
+  let authorization = req.headers.authorization
+  if (!authorization) return next(new Error('Unauthorized'))
+
+  let parts = authorization.split(' ')
+  let scheme = parts[0]
+  let auth = new Buffer(parts[1], 'base64').toString().split(':')
+  let user = auth[0]
+  let pass = auth[1]
+
+  authenticcateWithDatabase(user, pass, err => {
+    if (err) return next(err) // 告诉分配器出错了
+    
+    next() // 如果认证完成，不带参数调用next
+  })
+}
+
+// 显示管理面板的中间件
+function admin(req, res, next) {
+  switch (req.url) {
+    case '/':
+      res.end('try /users')
+      break
+    case '/users':
+      res.setHeader('Content-Type', 'application/json')
+      res.end(JSON.stringify(['mobi', 'pdf', 'txt']))
+  }
+}
+
+// 模拟一个数据库认证
+function authenticcateWithDatabase (user, pass, callback) {
+  //
 }
