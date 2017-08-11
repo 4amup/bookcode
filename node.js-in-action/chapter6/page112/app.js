316aadaa9f30cb8701bd0c4ed9db1976db90c9a8
wozhizuipc <490398311@qq.com>
2017-08-11 14:45:27 +0800

    done 完成了hello中间件

--- node.js-in-action/chapter6/page112/app.js
+++ node.js-in-action/chapter6/page112/app.js
@@ -3,6 +3,9 @@ const app = connect()
 
 // 使用中间件
 app.use(logger)
+app.use(hello)
+
+// 监听3000端口
 app.listen(3000, () => {
   console.log('http://localhost:3000')
 })
@@ -11,4 +14,10 @@ app.listen(3000, () => {
 function logger (req, res, next) {
   console.log('%s %s', req.method, req.url)
   next()
+}
+
+// 响应hello world的中间件
+function hello (req, res) {
+  res.setHeader('Content-Type', 'text/plain')
+  res.end('hello world')
 }
