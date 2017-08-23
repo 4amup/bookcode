43b713ff6d1c5e4a28af3d76ed09f3c2236a5fc0
lixiaochunoffice <490398311@qq.com>
2017-08-23 13:36:02 +0800

    update 测试使用app.locals设置的变量渲染模板

--- node.js-in-action/chapter8/page162/photo/app.js
+++ node.js-in-action/chapter8/page162/photo/app.js
@@ -21,6 +21,9 @@ nunjucks.configure('views', {
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'njk');
 
+// 测试res.locals和res.locals
+app.set('title', 'My Application');
+
 // uncomment after placing your favicon in /public
 app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
--- node.js-in-action/chapter8/page162/photo/routes/index.js
+++ node.js-in-action/chapter8/page162/photo/routes/index.js
@@ -3,7 +3,8 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  // res.render('index', { title: 'Express' });
+  res.render('index'); // 测试使用app.locals的变量
 });
 
 module.exports = router;
