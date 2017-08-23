4faf57d049b25d22b197e1c0b3cfc06f9e4a0604
lixiaochunoffice <490398311@qq.com>
2017-08-23 13:36:43 +0800

    update 测试完成，注释掉继续下一步

--- node.js-in-action/chapter8/page162/photo/app.js
+++ node.js-in-action/chapter8/page162/photo/app.js
@@ -22,7 +22,7 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'njk');
 
 // 测试res.locals和res.locals
-app.set('title', 'My Application');
+// app.set('title', 'My Application');
 
 // uncomment after placing your favicon in /public
 app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
--- node.js-in-action/chapter8/page162/photo/routes/index.js
+++ node.js-in-action/chapter8/page162/photo/routes/index.js
@@ -3,8 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  // res.render('index', { title: 'Express' });
-  res.render('index'); // 测试使用app.locals的变量
+  res.render('index', { title: 'Express' });
 });
 
 module.exports = router;
