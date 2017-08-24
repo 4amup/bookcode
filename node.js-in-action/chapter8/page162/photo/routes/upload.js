20a14cb26ff4fe4c8f3aed7b9ee7b636bbbac5cb
lixiaochunoffice <490398311@qq.com>
2017-08-24 20:31:55 +0800

    update 上传为解决
    找找以前写的代码吧。

--- node.js-in-action/chapter8/page162/photo/routes/upload.js
+++ node.js-in-action/chapter8/page162/photo/routes/upload.js
@@ -1,9 +1,22 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
+const fs = require('fs')
+const path = require('path')
+const formidable = require('formidable')
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
+/* GET upload page. */
+router.get('/', function (req, res, next) {
   res.render('upload', {title: 'Photo upload'});
 });
 
+// 上传
+router.post('/', function (req, res, next) {
+  let form = new formidable.IncomingForm()
+  form.uploadDir = "../images"
+  form.parse(req, (err, field, files) => {
+    res.writeHead(200, {'Content-Type': 'text/plain'})
+    res.write('received upload')
+    res.end(fields, files)
+  })
+})
 module.exports = router;
