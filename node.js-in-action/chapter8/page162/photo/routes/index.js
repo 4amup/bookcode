47a0f55d74513c321b78047744fd611cfe72dd3b
lixiaochunroom <490398311@qq.com>
2017-08-26 12:55:15 +0800

    update 使用中间件formidable上传文件

--- node.js-in-action/chapter8/page162/photo/routes/index.js
+++ node.js-in-action/chapter8/page162/photo/routes/index.js
@@ -1,9 +1,59 @@
-var express = require('express');
-var router = express.Router();
+// var express = require('express');
+// var router = express.Router();
 
+// /* GET home page. */
+// router.get('/', function(req, res, next) {
+//   res.render('index', { title: 'Express' });
+// });
+
+// module.exports = router;
+var express = require('express');
+var formidable = require('formidable');
+var router = express.Router();
+var fs =require('fs');
 /* GET home page. */
-router.get('/', function(req, res, next) {
+router.get('/', function(req, res) {
   res.render('index', { title: 'Express' });
+});
+router.post('/', function(req, res) {
+  //设置返回字符串编码
+  res.header( 'Content-Type','text/javascript;charset=utf-8');
+  //new一个formidable.IncomingForm();
+  var form = new formidable.IncomingForm();
+  //设置临时文件存放的路径
+  form.uploadDir = "./routes";
+  //设置上传数据的编码
+  form.encoding='utf-8';
+  //设置是否保持上传文件的拓展名
+  form.keepExtensions = true;
+  //文件上传过程中触发可以做上传进度查看
+  form.on('progress', function(bytesReceived, bytesExpected) {
+    if(bytesExpected>1024*1024*3){//bytesExpected为等待上传的文件的大小，超过大小就返回错手动触发error
+      this.emit('error',"文件过大")
+    };
+  });
+  //返回非文件的部分数据
+  form.on('field', function(name, value) {
+    console.log(name+" "+value)
+  });
+  //文件上传成功后触发
+  form.on('file', function(name, file) {
+    //成功上传，把临时文件移动到public文件夹下面
+    fs.renameSync(file.path, "./public/photos/" + file.name);
+  });
+  //流程正常处理
+  form.on('end',function(){
+    res.end('上传成功');
+  });
+  //出错
+  form.on('error',function(err){
+    res.end(err);
+  })
+  //执行文件上传任务
+  form.parse(req,function(){
+
+  });
+
 });
 
 module.exports = router;
--- node.js-in-action/chapter8/page162/photo/routes/upload.js
+++ node.js-in-action/chapter8/page162/photo/routes/upload.js
@@ -1,22 +1,59 @@
-const express = require('express');
-const router = express.Router();
-const fs = require('fs')
-const path = require('path')
-const formidable = require('formidable')
+var express = require('express');  
+var formidable = require('formidable');  
+var router = express.Router();  
+var fs =require('fs'); 
+
 
 /* GET upload page. */
 router.get('/', function (req, res, next) {
   res.render('upload', {title: 'Photo upload'});
 });
-
-// 上传
-router.post('/', function (req, res, next) {
-  let form = new formidable.IncomingForm()
-  form.uploadDir = "../images"
-  form.parse(req, (err, field, files) => {
-    res.writeHead(200, {'Content-Type': 'text/plain'})
-    res.write('received upload')
-    res.end(fields, files)
-  })
-})
+// POST文件上传
+router.post('/', function(req, res) {  
+  //设置返回字符串编码  
+  res.header( 'Content-Type','text/javascript;charset=utf-8');  
+  //new一个formidable.IncomingForm();  
+  var form = new formidable.IncomingForm();  
+  //设置临时文件存放的路径  
+  form.uploadDir = "./routes";  
+  //设置上传数据的编码  
+  form.encoding='utf-8';  
+  //设置是否保持上传文件的拓展名  
+  form.keepExtensions = true;  
+  //文件上传过程中触发可以做上传进度查看  
+  form.on('progress', function(bytesReceived, bytesExpected) {  
+    if(bytesExpected>1024*1024*3){//bytesExpected为等待上传的文件的大小，超过大小就返回错手动触发error  
+      this.emit('error',"文件过大")  
+    };  
+  });  
+  //返回非文件的部分数据  
+  form.on('field', function(name, value) {  
+    console.log(name+" "+value)  
+  });  
+  //文件上传成功后触发  
+  form.on('file', function(name, file) {  
+    if(file.type!='text/css'){//文件类型不是合法的  
+      this.emit('error',"不允许的类型");//手动触发error  
+      fs.unlink(file.path)//删掉临时文件  
+    }  
+    else {  
+      //成功上传，把临时文件移动到public文件夹下面  
+      fs.renameSync(file.path, "./public/" + file.name);  
+    }  
+  });  
+  //流程正常处理  
+  form.on('end',function(){  
+    res.end('上传成功');  
+  });  
+  //出错  
+  form.on('error',function(err){  
+    res.end(err);  
+  })  
+  //执行文件上传任务  
+  form.parse(req,function(){  
+  
+  });  
+  
+});  
+  
 module.exports = router;
